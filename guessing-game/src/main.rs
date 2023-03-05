61243bb86d9e0e694846eaa10ee3b3e13955054e
santtuniskanen <santtuniska02@gmail.com>
2023-03-05 10:19:18 +0200

    Fixing main.rs

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,9 +1,6 @@
 use std::io;
 
 fn main() {
-    let x = 5;
-    let y = 5;
-    println!("x multiplied by y equals {}", x * y);
     println!("Guess the number!");
 
     println!("Please input your guess.");
